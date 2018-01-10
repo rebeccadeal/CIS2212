b5ea8fe65804e5ff7226a9b703a806bec8af3ce7
Rebecca Deal <rebeccadeal@me.com>
2018-01-09 19:11:25 -0500

    Initial commit

---
+++ Week01Chap123/src/p1/FirstProgram.java
@@ -0,0 +1,18 @@
+/* Rebecca Deal
+ * CIS 2212 Tuesdays Spring 2018
+ * Class Demo 1 - Chapters 1, 2, 3
+ * Date: 1/9/18
+ */
+
+package p1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		// Our first code to execute
+		System.out.println(" Hello World! ");
+
+	}
+
+}
